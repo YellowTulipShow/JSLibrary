1aa572093954bc579e541cc6a1b924e5abafd6cd
YellowTulipShow <1426689530@qq.com>
2019-07-03 11:08:40 +0800

    dev complete Class.Inherit.js

---
+++ o-o-p/Class.Inherit.js
@@ -0,0 +1,52 @@
+/*
+    继承类实现
+    作者: YellowTulipShow
+    开源地址: https://github.com/YellowTulipShow/JSLibrary/tree/master/o-o-p/Class.Inherit.js
+    使用看同级目录下的 Class.Inherit.Use.html
+*/
+(function() {
+    function deepCopy(p, c, b) {
+        var c = c || {};
+        for (var i in p) {
+            if (typeof p[i] === 'object') {
+                c[i] = (p[i].constructor === Array) ? [] : {};
+                deepCopy(p[i], c[i]);
+            } if (typeof (p[i]) === "function") {
+                c[i] = p[i].bind(b);;
+            } else {
+                c[i] = p[i];
+            }
+        }
+        return c;
+    }
+    function Unrealized() {
+        throw "throw Unrealized Method Exception!!!";
+    }
+    var Class = function() {};
+    Class.Inherit = function(property) {
+        var baseF = this !== Class ? this : null;
+        function F() {
+            if (!initializing) {
+                if (baseF) {
+                    this._base_ = deepCopy(baseF.prototype, {}, this);
+                }
+                this.__init__.apply(this, arguments);
+            }
+        }
+        if (baseF) {
+            initializing = true;
+            F.prototype = new baseF();
+            F.prototype.constructor = F;
+            initializing = false;
+        }
+        F.Inherit = arguments.callee;
+        for (var name in property) {
+            if (!property.hasOwnProperty(name)) {
+                continue;
+            }
+            F.prototype[name] = property[name];
+        }
+        return F;
+    };
+    this.Class = Class;
+})();
