3cab606dbe0cd32a01256978379fbb1707b35f65
YellowTulipShow <1426689530@qq.com>
2019-07-03 17:31:27 +0800

    dev test

--- o-o-p/Class.Inherit.js
+++ o-o-p/Class.Inherit.js
@@ -5,49 +5,73 @@
     使用看同级目录下的 Class.Inherit.Use.html
 */
 (function() {
-    function deepCopy(p, c, b) {
-        var c = c || {};
-        for (var i in p) {
-            if (typeof p[i] === 'object') {
-                c[i] = (p[i].constructor === Array) ? [] : {};
-                deepCopy(p[i], c[i]);
-            } if (typeof (p[i]) === "function") {
-                c[i] = p[i].bind(b);;
-            } else {
-                c[i] = p[i];
+    function deepCopy(target, sourceProperty, config) {
+        var t = target || {};
+        var s = sourceProperty;
+        var c = config || {
+            base: null,
+            isContinue: function(sourceProperty, key) {},
+        };
+        for (var i in s) {
+            if (c.isContinue && c.isContinue(s, i)) {
+                continue;
             }
+            if (typeof s[i] === 'object') {
+                t[i] = (s[i].constructor === Array) ? [] : {};
+                deepCopy(s[i], t[i]);
+                continue;
+            }
+            if (typeof (s[i]) === "function") {
+                if (c.base) {
+                    t[i] = s[i].bind(c.base);;
+                    continue;
+                }
+            }
+            t[i] = s[i];
         }
-        return c;
+        return t;
     }
+
     function Unrealized() {
         throw "throw Unrealized Method Exception!!!";
     }
     var Class = function() {};
-    Class.Inherit = function(property) {
+    Class.Inherit = function(property, functionName) {
         var baseF = this !== Class ? this : null;
         function F() {
-            if (!initializing) {
-                if (baseF) {
-                    this._base_ = deepCopy(baseF.prototype, {}, this);
-                }
-                this.__init__.apply(this, arguments);
-            }
-        }
-        if (baseF) {
-            initializing = true;
-            var baseFcase = new baseF();
-            F.prototype = baseFcase;
-            F.prototype._super_ = baseFcase;
-            F.prototype.constructor = F;
-            initializing = false;
+            // console.log(this);
+            // this.__init__.apply(this.F, arguments);
+            this.__init__(arguments);
         }
         F.Inherit = arguments.callee;
-        for (var name in property) {
-            if (!property.hasOwnProperty(name)) {
-                continue;
+        F.Inherit = Class.Inherit;
+
+        if (baseF) {
+            F.prototype = deepCopy({}, baseF.prototype);
+            F.prototype.constructor = F;
+            F.prototype._base_ = deepCopy({}, baseF.prototype, {
+                base: F,
+            });
+            F.prototype._base_.constructor = F;
+            for (var key in property) {
+                if (!property.hasOwnProperty(key)) {
+                    continue;
+                }
+                F.prototype[key] = property[key];
             }
-            F.prototype[name] = property[name];
+        } else {
+            F.prototype = property;
+            if (!F.prototype.__init__) {
+                F.prototype.__init__ = function() {};
+            }
+            F.prototype.constructor = F;
+            F.prototype._base_ = {};
+            F.prototype._base_.constructor = F;
         }
+        if (functionName) {
+            F.prototype._functionName_ = functionName;
+        }
+        F._type_ = F;
         return F;
     };
     this.Class = Class;
